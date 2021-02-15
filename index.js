f6d49df1e93957d8dedd99b4a3a22d0da720b713
Una LEstrange <lestrange.una@itsligo.ie>
2021-02-15 21:26:20 +0000

    added bootstrap and some sytling, added 404 for people not found

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000
 
 // set up handlebars view engine
 var handlebars = require('express-handlebars')
-.create({ defaultLayout:'main' });
+    .create({ defaultLayout: 'main' });
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
@@ -31,12 +31,12 @@ app.get('/', function (req, res) {
 });
 
 
-app.get('/about',  (req, res) => {
-    
+app.get('/about', (req, res) => {
+
     res.render('about');
 });
 
-app.get('/contact',  (req, res) => {
+app.get('/contact', (req, res) => {
     res.render('contact');
 });
 
@@ -44,15 +44,36 @@ app.get('/contact',  (req, res) => {
 // a file - this is just to show how templates work and how we can pass them 
 // data
 
-var data = {"foil" : { "name": "foil",
-         "dob": "01/01/1998",
+var data = {
+    "foil": {
+        "name": "foil",
+        "dob": "01/01/3030",
         "imageurl": "/images/foilimage1.png",
-        "hobbies": ["Jokes", "Gags", "Stand up"]},
-        "arms" :  { "name": "arms",
+        "hobbies": ["Jokes", "Gags", "Stand up"]
+    },
+    "arms": {
+        "name": "arms",
         "dob": "03/05/1995",
-       "imageurl": "/images/armsimage1.png"},
-        "hog" : { "name": "hog",
-        "imageurl": "/images/hogimage1.png"} }
+        "imageurl": "/images/armsimage1.png"
+    },
+    "hog": {
+        "name": "hog",
+        "imageurl": "/images/hogimage1.png"
+    },
+
+    "hog2": {
+        "name": "hog",
+        "imageurl": "/images/hogimage1.png"
+    },
+    "hog3": {
+        "name": "hog",
+        "imageurl": "/images/hogimage1.png"
+    },
+    "hog4": {
+        "name": "hog",
+        "imageurl": "/images/hogimage1.png"
+    }
+}
 
 // app.get('/foil', (req,res) =>
 //        res.render('person', {person: data.foil} ))
@@ -63,33 +84,40 @@ var data = {"foil" : { "name": "foil",
 // app.get('/hog', (req,res) =>
 //        res.render('person', {person: data.hog} ))
 
-app.get('/personlist', (req,res) =>
-    res.render('personlist', { personlist: data }))
+app.get('/personlist', (req, res) =>
+        res.render('personlist', { personlist: data }))
 
 app.get('/personlist/:name', (req, res) => {
 
-var name = req.params.name;
+            var name = req.params.name;
 
-res.render('person', {person: data[name]})
-})
+            if (data[name] == null) {
+                res.render('404'); // could also have a special page for person not found
+            }
+            else {
+                res.render('person', { person: data[name] })
+            }
+
+
+        })
 
 
 
 
 // custom 404 page
-app.use( (req, res) => {
-    res.type('text/plain');
-    res.status(404);
-    res.send('404 - Not Found');
-});
+app.use((req, res) => {
+            res.type('text/plain');
+            res.status(404);
+            res.send('404 - Not Found');
+        });
 
-// custom 500 page
-app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.type('text/plain');
-    res.status(500);
-    res.send('500 - Server Error');
-});
+    // custom 500 page
+    app.use((err, req, res, next) => {
+        console.error(err.stack);
+        res.type('text/plain');
+        res.status(500);
+        res.send('500 - Server Error');
+    });
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`))
